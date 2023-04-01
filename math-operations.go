07ff579b22f7cd248a26414c4b9ecbe02dc5625e
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-01 12:40:26 +0800

    Operasi Matematika

---
+++ math-operations.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main(){
+	var result = 10 + 10
+	fmt.Println(result)
+
+	var a = 23
+	var b = 22
+	var c = a * b
+	fmt.Println(c)
+
+	var i = 10
+	// Augmented Assaigments
+	i += 12 // i = i + 10
+
+	fmt.Println(i)
+
+	// Unary operator
+	i++
+	fmt.Println(i)
+
+	var negative = -10
+	var positive = 10
+	fmt.Println(negative)
+	fmt.Println(positive)
+
+
+}
+
+/*
+	Unary operator yaitu operasi kesebuah variable
+*/
