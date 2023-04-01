613d59a650fe5ace4a63aee822a64c0b8e144259
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-01 10:10:04 +0800

    Program Hello World

---
+++ helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World")
+}
+/* 
+	go build namafile.go 
+	untuk compile
+
+	go run namafile.go
+	untuk run tanpa compile 
+*/
+
+/*
+	export PATH=$PATH:/usr/local/go/bin
+*/
