dce4fcbaabb3199d247a60b6760672dd06d24dac
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-01 11:01:42 +0800

    Type data String

---
+++ string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Luna")
+	fmt.Println("Meizaluna")
+	fmt.Println("Meizaluna Wulandari")
+	fmt.Println(len("Meizaluna Wulandari"))
+	fmt.Println("Meizaluna Wulandari"[0])
+}
+/*
+	string harus pakai petik dua(")
+	len untuk menghitung jumlah karakter
+	[0] untuk mengambil karakter tentu dimulai dari 0 dan akan menghasilkan representasi byte dari string yang diambil
+
+*/
