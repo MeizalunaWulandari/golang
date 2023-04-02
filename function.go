d2fbc22fb54c258b7cee5f8cfa687ed49438a0cc
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-02 11:50:25 +0800

    Function

---
+++ function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func sayHello(){
+	fmt.Println("Helloworld")
+}
+
+func main(){
+	sayHello()
+}
+
+/*
+	function adalah blok kode yang sengaja dibuat dalam program agar bisa digunakan secara berulang
+	func nameFunction() adalah cara untuk membuat function
+	setelah membuat function jika ingin mengeksekusinya cukup dengan menulis nama functionnya namaFunction()
+*/
