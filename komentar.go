8ee136bae3130de515655652afa4ded0839783fe
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-04 09:44:59 +0800

    Komentar

---
+++ komentar.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/**
+ * Komentar multi line bisa digunakan diluar function
+ * bertujuan tuntuk membuat suatu dokumentasi mengenai 
+ * fungsi dari function tersebut	
+*/
+
+func main(){
+	// Ini komentar satu baris, biasanya digunakan didalam function
+}
